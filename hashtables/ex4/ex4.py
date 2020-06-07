43de3cc04ef69aa8b176bfee153cebc25453058d
kika <anamonteiro430@gmail.com>
2020-06-07 22:04:13 +0100

    ex4 one test passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,23 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    count={}
+    result=[]
+    a = [abs(i) for i in a]
+    print(a)
+    
+    for i in a:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    print(count)
 
+    for key in count:
+        if count[key] == 2:
+            result.append(key)
     return result
 
+
+        
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
